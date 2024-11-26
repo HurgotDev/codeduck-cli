13884b773a04412de7226b0465fe0d71dfbf5f8d
Hugo Gutierrez <hugo.gutierrezm28@gmail.com>
2024-11-26 11:01:31 -0500

    fix: 🐛 Git clone

--- src/lib/repos.ts
+++ src/lib/repos.ts
@@ -11,7 +11,7 @@ export function cloneRepository(repoUrl: string) {
   const sanitizedRepoUrl = repoUrl.replace("git@github.com:", "").replace(".git", "");
   const [projectName, repositoryName] = sanitizedRepoUrl.split("/");
 
-  const projectPath = expandBasePath(path.join(config.basePath, projectName));
+  const projectPath = `"${expandBasePath(path.join(config.basePath, projectName))}"`;
   const repositoryPath = path.join(projectPath, repositoryName);
 
   execSync(`git clone ${repoUrl} ${repositoryPath}`);
